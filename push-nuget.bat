770b89478e8ffea24e16e3049fe5a07708a874c8
romkatv <roman.perepelitsa@gmail.com>
2018-04-30 17:04:13 +0200

    Move the example to its own solution.

--- push-nuget.bat
+++ push-nuget.bat
@@ -4,9 +4,10 @@ REM This script must be run from the root of NLogEvents project (the current
 REM directory must contain NLogEvents.sln). It builds the whole solution and
 REM pushes a new version of NLog.Events to nuget.org.
 
-"C:\Program Files (x86)\Microsoft Visual Studio\2017\Professional\Common7\IDE\devenv.com" /rebuild Release NLogEvents.sln || goto :error
+SET DEVENV="C:\Program Files (x86)\Microsoft Visual Studio\2017\Professional\Common7\IDE\devenv.com"
 
 cd NLogEvents || goto :error
+%DEVENV% /rebuild Release NLogEvents.sln || goto :error
 if exist *.nupkg (del *.nupkg || goto :error)
 nuget pack NLogEvents.csproj -IncludeReferencedProjects -Prop Configuration=Release || goto :error
 nuget push -Source https://nuget.org *.nupkg || goto :error
