1d3f268e75241f43eee0d25e7ecf628939d6a47b
romkatv <roman.perepelitsa@gmail.com>
2018-04-30 16:47:49 +0200

    Initial version.

---
+++ NLogEvents/Targets.cs
@@ -0,0 +1,21 @@
+﻿using NLog;
+using NLog.Config;
+using NLog.Targets;
+
+namespace NLogEvents
+{
+    /// <summary>
+    /// NLog target that triggers `NLogEvents.Events.OnLog`.
+    /// </summary>
+    [Target("OnLogEvent")]
+    class OnLogEvent : TargetWithLayout
+    {
+        /// <summary>
+        /// Defines the value of the first argument of `NLogEvents.Events.OnLog`.
+        /// The default is `null`.
+        /// </summary>
+        public string Event { get; set; }
+
+        protected override void Write(LogEventInfo info) => Events.FireOnLog(Event, info);
+    }
+}
